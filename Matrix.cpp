19d41afb3afce3cb0ab2db82ad76d54421c22447
nolan freeman abbott <nolan@freemanabbott.com>
2024-05-25 07:51:59 -0500

    initial commit with basic Matrix implementation

---
+++ Matrix.cpp
@@ -0,0 +1,53 @@
+#include "Matrix.h"
+
+namespace ns 
+{
+
+template <typename T, std::size_t nRows, std::size_t nColumns>
+Matrix<T, nRows, nColumns>::Matrix()
+{ }
+
+template <typename T, std::size_t nRows, std::size_t nColumns>
+Matrix<T, nRows, nColumns>::Matrix(std::initializer_list<std::initializer_list<T>> list)
+{ 
+    if (list.size() != nRows)
+        throw std::invalid_argument("Initializer list does not match intended number of rows.");
+
+    for (std::size_t i = 0; i < nRows; ++i)
+    {
+        if (list.begin()[i].size() != nColumns)
+            throw std::invalid_argument("Initializer list does not match intended number of columns.");
+
+        std::copy(list.begin()[i].begin(), list.begin()[i].end(), elements[i].begin());
+    }
+}
+
+template <typename T, std::size_t nRows, std::size_t nColumns>
+Matrix<T, nRows, nColumns>::~Matrix()
+{ }
+
+template <typename T, std::size_t nRows, std::size_t nColumns>
+const T& Matrix<T, nRows, nColumns>::at(std::size_t row, std::size_t col) const
+{
+    if (row > nRows) 
+        throw std::out_of_range("Row index exceeds indexable range.");
+
+    if (row <= 0) 
+        throw std::out_of_range("Row index below indexable range.");
+
+    if (col > nColumns)
+        throw std::out_of_range("Column index exceeds indexable range.");
+
+    if (col <= 0)
+        throw std::out_of_range("Column index below indexable range.");
+
+    return elements[row-1][col-1];
+}
+
+template <typename T, std::size_t nRows, std::size_t nColumns>
+T& Matrix<T, nRows, nColumns>::at(std::size_t row, std::size_t col)
+{
+    return const_cast<T&>(const_cast<const Matrix&>(*this).at(row, col));
+}
+
+}
---
+++ Matrix.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <initializer_list>
+#include <array>
+
+namespace ns 
+{
+
+template <typename T, std::size_t nRows, std::size_t nColumns>
+class Matrix
+{
+    public:
+        Matrix();
+        Matrix(std::initializer_list<std::initializer_list<T>> list);
+        ~Matrix();
+
+        const T& at(std::size_t row, std::size_t col) const;
+        T& at(std::size_t row, std::size_t col);
+    
+    public:
+
+    private:
+
+    private:
+        std::array<std::array<T, nColumns>, nRows> elements;
+
+};
+
+} 
+
+#include "Matrix.cpp"
