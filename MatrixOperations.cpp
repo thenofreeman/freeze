8c4bbf9d1b74cfc83473e3943846b5c85ac7e4e0
nolan freeman abbott <nolan@freemanabbott.com>
2024-05-25 13:45:51 -0500

    add comments for new matrix operation methods

--- MatrixOperations.cpp
+++ MatrixOperations.cpp
@@ -127,6 +127,26 @@ template <typename T, std::size_t nRows, std::size_t nColumns>
     Matrix<T, nRows, nColumns> 
         transpose(const Matrix<T, nRows, nColumns>& matrix);
 
+template <typename T, std::size_t nRows, std::size_t nColumns>
+    Matrix<T, nRows, nColumns> 
+        cross(const Matrix<T, nRows, nColumns>& lhs, const Matrix<T, nRows, nColumns>& rhs);
+
+template <typename T, std::size_t nRows, std::size_t nColumns>
+    Matrix<T, nRows, nColumns> 
+        determinant(const Matrix<T, nRows, nColumns>& matrix);
+
+template <typename T, std::size_t nRows, std::size_t nColumns>
+    Matrix<T, nRows, nColumns> 
+        trace(const Matrix<T, nRows, nColumns>& matrix);
+
+template <typename T, std::size_t nRows, std::size_t nColumns, typename Other>
+    std::ostream&
+        operator<<(std::ostream& os, const Matrix<T, nRows, nColumns>& rhs)
+
+template <typename T, std::size_t nRows, std::size_t nColumns>
+    Matrix<T, nRows, nColumns> 
+        operator==(const Matrix<T, nRows, nColumns>& lhs, const Matrix<T, nRows, nColumns>& rhs);
+
 */
 
 };
