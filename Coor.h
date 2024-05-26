a4313f2974a82b1974197cecfd4f58095f7f6cfe
nolan freeman abbott <nolan@freemanabbott.com>
2024-05-26 08:51:10 -0500

    write coor helper structures

---
+++ Coor.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace ns
+{
+
+template <typename T>
+struct Coor2
+{
+    T x;
+    T y;
+
+};
+
+template <typename T>
+struct Coor3
+{
+    T x;
+    T y;
+    T z;
+
+};
+
+}
